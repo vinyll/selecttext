9e006397ef1767fa3948e11e3d64541cfd816a2d
Vincent Agnano <v.agnano@protonmail.com>
2019-10-22 21:01:20 +0200

    moved code into a separate library

---
+++ selecttext.js
@@ -0,0 +1,45 @@
+class SelectText {
+  constructor(container) {
+    this.selection = []
+    this.container = container
+    this.words = container.textContent.trim().split(/([\W]+)/)
+    this.render()
+  }
+
+  renderSpan(text, index) {
+    return `<span
+      index=${index}
+      ${this.selection.length && this.selection[0] <= index && index < this.selection[1] ? `selected` : ''}
+      >${text}</span>`
+  }
+
+  addSelection(index, length) {
+    if(index == this.selection[0]) return this.selectionChanged([])
+    // Select next words
+    const selection = [this.selection[0]]
+    if(!this.selection.length || index <= this.selection[0]) selection[0] = index
+    selection[1] = index + length - 1
+    this.selectionChanged(selection)
+  }
+
+  selectionChanged(selection) {
+    this.selection = selection
+    this.onSelect && this.onSelect(selection)
+    this.render()
+  }
+
+  render() {
+    let index = 0
+    this.container.innerHTML = this.words.map(w => {
+      const span = this.renderSpan(w, index)
+      index += w.length
+      return span
+    }).join('')
+
+    this.container.querySelectorAll('span').forEach(span => span.addEventListener('click', (event) => {
+      const span = event.target
+      const index = Number(span.getAttribute('index'))
+      this.addSelection(index, span.textContent.length)
+    }))
+  }
+}
