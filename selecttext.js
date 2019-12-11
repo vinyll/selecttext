902bb0a2aca45afc7bef6b6cbf80f4098e97f57f
Vincent Agnano <v.agnano@protonmail.com>
2019-12-11 18:49:42 +0100

    accept alphabet character from various cultures

--- selecttext.js
+++ selecttext.js
@@ -1,6 +1,6 @@
 export default class SelectText {
   constructor(container) {
-    this.regex = /([0-9A-zÀ-ú@#$%€±°÷≤µ˜∫√ç≈åß∂ƒ©˙∆˚®™£]+)/
+    this.regex = /([0-9A-Za-zœ-ŸÀ-ʯΆ-ῼŠšŽžŒ$%€µß∂ƒ]+)/
     this.selection = []
     this.container = container
     this.words = container.textContent.trim().split(this.regex)
